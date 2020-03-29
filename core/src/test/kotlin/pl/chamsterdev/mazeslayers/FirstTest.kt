f549103226533e47484cf393b6e7c06ca8549c24
Piotr Chelmicki <Piotr.Chelmicki@jeppesen.com>
2020-03-29 11:41:59 +0200

    Unit tests setup [not working]

---
+++ core/src/test/kotlin/pl/chamsterdev/mazeslayers/FirstTest.kt
@@ -0,0 +1,11 @@
+package pl.chamsterdev.mazeslayers
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class FirstTest {
+    @Test
+    fun `my first test`() {
+        assertEquals(1,1)
+    }
+}
